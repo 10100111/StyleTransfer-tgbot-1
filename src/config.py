f5f1f062f3694f2e2e793c548bef56025896d001
Irina <92697078+10100111@users.noreply.github.com>
2022-09-15 15:41:19 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 TOKEN = '1106296054:AAGWR7iJJqb0QJF3uraBw1OIK1x0ZEzpfiI'
 model_path = 'src/model/model_state.pth'
 endpoint = 'https://styletransfer-bot.herokuapp.com/'
-herokuapi = '775acb09-665d-45a5-a77c-a976a8b4f877'
+herokuapi = '8512c7d2-1563-4375-8842-1eb3410ae65c'
 
